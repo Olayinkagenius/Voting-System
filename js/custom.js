445223d809e8ecdfcf0f962f5b60d42c15e585cd
Benjamin Olapade <69037600+De-ORaCle@users.noreply.github.com>
2021-10-01 03:34:40 +0100

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -44,3 +44,43 @@ $(document).ready(function() {
     new WOW({ mobile: false }).init();
 
 });
+
+// Get the container element
+var btnContainer = document.getElementById("myDIV");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+
+        // If there's no active class
+        if (current.length > 0) {
+            current[0].className = current[0].className.replace(" active", "");
+        }
+
+        // Add the active class to the current/clicked button
+        this.className += " active";
+    });
+}
+var btnContainer2 = document.getElementById("hamDIV");
+
+// Get all buttons with class="btn" inside the container
+var btns2 = btnContainer2.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns2.length; i++) {
+    btns2[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("hactive");
+
+        // If there's no active class
+        if (current.length > 0) {
+            current[0].className = current[0].className.replace(" hactive", "");
+        }
+
+        // Add the active class to the current/clicked button
+        this.className += " hactive";
+    });
+}
